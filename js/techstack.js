1e8241389006eadace28b77d2c10f373d17fe109
ChrisWen430 <chriswen430@gmail.com>
2025-11-27 05:26:01 +0000

    Site updated: 2025-11-27 05:26:01

---
+++ js/techstack.js
@@ -0,0 +1,100 @@
+window.techIcons = {
+  java: { text: "Java", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/java-icon.png",url: "https://dev.java/"},
+  spring: { text: "Spring", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/spring-icon.png",url: "https://spring.io/" },
+  springMVC: { text: "Spring MVC", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203249010.png",url: "https://spring.io/guides/gs/serving-web-content" },
+  springBoot: { text: "Spring Boot", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/springboot-icon.png",url: "https://spring.io/projects/spring-boot" },
+  springBootOauth2: { text: "Spring Boot Oauth2", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010222950192.png",url: "https://spring.io/guides/tutorials/spring-boot-oauth2" },
+  springSecurity: { text: "Spring Security", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203221479.png",url: "https://spring.io/projects/spring-security" },
+  springCloud: { text: "Spring Cloud", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203338706.png",url: "https://spring.io/projects/spring-cloud" },
+
+  shiro: { text: "Apache Shiro", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203410481.png",url: "https://shiro.apache.org/" },
+
+  zookeeper: { text: "ZooKeeper", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/zookeeper-icon.png",url: "https://zookeeper.apache.org/" },
+  dubbo: { text: "Dubbo", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/dubbo-icon.png",url: "https://dubbo.apache.org/en/index.html" },
+  
+  redis: { text: "Redis", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/redis-icon.png",url: "https://redis.io/" },
+  rabbitMQ: { text: "RabbitMQ", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/rabbitmq-icon.png",url: "https://www.rabbitmq.com/" },
+  
+  mysql: { text: "MySQL", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/mysql-icon.png",url: "https://www.mysql.com/" },
+  sqlServer: { text: "SQL Server", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203443326.png",url: "https://www.microsoft.com/en-au/sql-server/sql-server-downloads#Get-started" },
+  oracle: { text: "Oracle", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010235638797.png",url: "https://www.oracle.com/au/database/" },
+  oceanBase: { text: "OceanBase", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010235721537.png",url: "https://en.oceanbase.com/" },
+  postgresql: { text: "PostgreSQL", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012125049422.png",url: "https://www.postgresql.org/" },
+
+  jwt: { text: "JWT", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007102922283.png",url: "https://en.wikipedia.org/wiki/JSON_Web_Token" },
+
+  vue2: { text: "Vue2", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007102248001.png",url: "https://v2.vuejs.org/" },
+  vue3: { text: "Vue3", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007102228900.png",url: "https://vuejs.org/" },
+  react: { text: "React", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012124344185.png",url: "https://react.dev/" },
+  elementUI: { text: "ElementUI", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007102136409.png",url: "https://element.eleme.io/" },
+  layUI: { text: "LayUI", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010225724505.png",url: "https://layui.dev/" },
+  postCSS: { text: "PostCSS", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012115422929.png",url: "https://postcss.org/" },
+  tailwindCSS: { text: "TailwindCSS", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012124607689.png",url: "https://tailwindcss.com/" },
+  
+  nextjs: { text: "Next js", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012124923514.png",url: "https://github.com/nextjs" },
+
+  jquery: { text: "jQuery", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010235915147.png",url: "https://jquery.com/" },
+  css: { text: "CSS", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007113303445.png",url: "https://en.wikipedia.org/wiki/CSS" },
+  html: { text: "HTML", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251007113337442.png",url: "https://en.wikipedia.org/wiki/HTML" },
+  
+  docker: { text: "Docker", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203544705.png",url: "https://www.docker.com/" },
+  dockerCompose: { text: "Docker Compose", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012125342220.png",url: "https://docs.docker.com/compose/" },
+  k8s: { text: "Kubernetes", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203606709.png",url: "https://kubernetes.io/" },
+
+  rancher: { text: "Rancher", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203719328.png",url: "https://www.rancher.com/" },
+  jenkins: { text: "Jenkins", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203741570.png",url: "https://www.jenkins.io/" },
+
+  git: { text: "Git", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203823259.png",url: "https://git-scm.com/" },
+  gitlab: { text: "GitLab", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203921460.png",url: "https://about.gitlab.com/" },
+  github: { text: "GitHub", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010203957166.png",url: "https://github.com/" },
+  githubAction: { text: "GitHub Action", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012125513433.png",url: "https://github.com/features/actions" },
+
+  nginx: { text: "Nginx", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204200149.png",url: "https://nginx.org/" },
+  tomcat: { text: "Apache Tomcat", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204245423.png",url: "https://tomcat.apache.org/" },
+  netty: { text: "Netty", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204403163.png",url: "https://netty.io/" },
+
+  linux: { text: "Linux", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204435948.png",url: "https://en.wikipedia.org/wiki/Linux" },
+  zeebe: { text: "Zeebe", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010222341721.png",url: "https://camunda.com/platform/zeebe/" },
+  flowable: { text: "Flowable", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010224106229.png",url: "https://www.flowable.com/open-source/docs/bpmn/ch02-GettingStarted" },
+  
+  xxlJob: { text: "XXL-JOB", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010234648810.png",url: "https://www.xuxueli.com/xxl-job/" },
+  horae: { text: "Horae", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010234906736.png",url: "" },
+
+  eureka: { text: "XXL-JOB", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010235055087.png",url: "https://cloud.spring.io/spring-cloud-netflix/reference/html/" },
+  solar: { text: "Solar", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010234906736.png",url: "https://developer.hashicorp.com/consul/commands/services/register" },
+
+  maven: { text: "Maven", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204517477.png",url: "https://maven.apache.org/" },
+  gradle: { text: "Gradle", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204618759.png",url: "https://gradle.org/" },
+  npm: { text: "NPM", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251010204714042.png",url: "https://docs.npmjs.com/downloading-and-installing-node-js-and-npm" },
+
+  aliyunECS: { text: "Aliyun ECS", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012115742724.png",url: "https://cn.aliyun.com/product/ecs" },
+  aliyunOSS: { text: "Aliyun OSS", icon: "https://raw.githubusercontent.com/wenyupeng/pic-lib/main/blog/20251012115946723.png",url: "https://cn.aliyun.com/product/oss" },
+
+};
+
+window.renderTechStack = function (containerId, stackList) {
+  console.log('renderTechStack triggered');
+  const container = document.getElementById(containerId);
+  if (!container) return;
+
+  container.innerHTML = stackList.map(key => {
+    const tech = window.techIcons[key];
+    if (!tech) return '';
+    return `
+      <div style="
+        flex: 1 1 120px; 
+        display: flex; 
+        align-items: center; 
+        justify-content: center;
+      ">
+        <a href="${tech.url}" target="_blank">
+          <img 
+            src="${tech.icon}" 
+            alt="${tech.text}" 
+            style="width: 80px; height: 80px; object-fit: contain;"
+          />
+        </a>
+      </div>
+    `;
+  }).join('');
+};
