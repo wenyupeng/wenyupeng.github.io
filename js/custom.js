74accf4ac9fe1ec40442499363b23d8783be3f28
ChrisWen430 <chriswen430@gmail.com>
2026-04-27 12:18:15 +0000

    Site updated: 2026-04-27 12:18:14

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const linkElement = document.querySelector('.post-copyright__type');
+  if (linkElement) {
+    linkElement.remove();
+  }
+});
